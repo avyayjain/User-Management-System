af237203c1d92d5ba025baf9b934faed49e1b47d
avyay jain <avyay.jain2001@gmail.com>
2023-06-10 22:40:04 +0530

    changed constants.py

--- src/common/utils/constants.py
+++ src/common/utils/constants.py
@@ -1,36 +1,24 @@
 import os
-# import dotenv
 
-# dotenv.load_dotenv()
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 600
-JWT_SUBJECT = "access"
-JWT_ALGORITHM = "HS256"
+TEST_URL="http://0.0.0.0:8000"
 
 DATABASE_PASS = os.getenv("DATABASE_PASS")
 DATABASE_USER = os.getenv("DATABASE_USER")
 DATABASE_DB = os.getenv("DATABASE_DB")
 DATABASE_URL = os.getenv("DATABASE_URL")
-#
-# DB_CONNECTION_LINK = "postgres://{}:{}@{}/{}".format(
-#     DATABASE_USER,
-#     DATABASE_PASS,
-#     DATABASE_URL,
-#     DATABASE_DB,
-# )
-
 DB_CONNECTION_LINK = "postgresql://{}:{}@{}/{}".format(
-    "postgres",
-    "1234",
-    "127.0.0.1:5432",
-    "yeh_zindagi",
+    DATABASE_USER,
+    DATABASE_PASS,
+    DATABASE_URL,
+    DATABASE_DB,
+)
+ASYNC_DB_CONNECTION_LINK = "postgresql+asyncpg://{}:{}@{}/{}".format(
+    DATABASE_USER,
+    DATABASE_PASS,
+    DATABASE_URL,
+    DATABASE_DB,
 )
 
-ASYNC_DB_CONNECTION_LINK = "postgresql+asyncpg://{}:{}@{}/{}".format(
-    "postgres",
-    "1234",
-    "127.0.0.1:5432",
-    "yeh_zindagi",
-)
+BYTES_PER_CHUNK = 1000
+
+FILE_FOLDER_PATH = os.path.join(os.getcwd(), 'files')
